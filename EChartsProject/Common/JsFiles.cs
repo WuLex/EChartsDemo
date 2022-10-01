d8a45ccce3f574180c3d00234094586a686416b7
wulex <827937686@qq.com>
2022-10-01 22:35:01 +0800

    更新

--- EChartsProject/Common/JsFiles.cs
+++ EChartsProject/Common/JsFiles.cs
@@ -4,7 +4,7 @@
     {
         private static string ver = "6.0";
 
-        public static string ECharts = "../../lib/echarts/echarts.js";
+        public static string ECharts = "../../lib/echarts/dist/echarts.js";
         public static string UserReport = "../../js/UserReport.js?ver=" + ver;
         public static string UserColumnReport = "../../js/UserColumnReport.js?ver=" + ver;
         public static string UserLineReport = "../../js/UserLineReport.js?ver=" + ver;
--- EChartsProject/Program.cs
+++ EChartsProject/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddScoped<ISqlSugarClient>(o =>
 {
     return new SqlSugar.SqlSugarClient(new SqlSugar.ConnectionConfig()
     {
-        ConnectionString = "Data Source=.;Initial Catalog=eUI;Persist Security Info=True;User ID=sa;Password=**********;Pooling=False;MAX Pool Size=2000;Min Pool Size=1;Connection Lifetime=30;",//必填, 数据库连接字符串
+        ConnectionString = "Data Source=.;Initial Catalog=eUI;Persist Security Info=True;User ID=sa;Password=wu199010;Pooling=False;MAX Pool Size=2000;Min Pool Size=1;Connection Lifetime=30;",//必填, 数据库连接字符串
         DbType = DbType.SqlServer,//必填, 数据库类型
         IsAutoCloseConnection = true,//默认false, 时候知道关闭数据库连接, 设置为true无需使用using或者Close操作
         InitKeyType = SqlSugar.InitKeyType.SystemTable//默认SystemTable, 字段信息读取, 如：该属性是不是主键，标识列等等信息
